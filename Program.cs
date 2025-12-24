48b6d8f5d6514d7eafaa68962ccd1bca408bdf73
uman_ia <iauman@activebc.ru>
2025-12-24 23:19:12 +0300

    module 4

--- Program.cs
+++ Program.cs
@@ -5,16 +5,34 @@ public class Program
 {
 	public static void Main()
 	{
-        System.Console.WriteLine("Введите название книги");
+		System.Console.WriteLine("Введите название книги");
 		string? name = Console.ReadLine();
-        System.Console.WriteLine("Введите автора книги");
+		System.Console.WriteLine("Введите автора книги");
 		string? author = Console.ReadLine();
-        System.Console.WriteLine("Введите год издания");
+		System.Console.WriteLine("Введите год издания");
 		string? year = Console.ReadLine();
-        System.Console.WriteLine("Введите ISBN");
-        string? isbn = Console.ReadLine();
-        Int32 yearNum = Convert.ToInt32(year);
+		System.Console.WriteLine("Введите ISBN");
+		string? isbn = Console.ReadLine();
+		Int32 yearNum = Convert.ToInt32(year);
 
 		Console.WriteLine($"Автор: {author}, название книги: {name}, год издания: {yearNum}, ISBN: {isbn}");
+
+		System.Console.WriteLine("Что Вы хотите сделать?");
+		string? action = Console.ReadLine();
+		switch (action)
+		{
+			case "Добавить книгу":
+				System.Console.WriteLine("Добавить книгу");
+				break;
+			case "Показать книгу":
+				System.Console.WriteLine("Показать книгу");
+				break;
+			case "Выйти":
+				System.Console.WriteLine("Выйти");
+				break;
+			default:
+				System.Console.WriteLine("Вы ввели что-то не то");
+				break;
+		}
 	}
 }
--- obj/Debug/net8.0/HomeLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d419bb3ca33ddfdafb83bdd6d14519eb93b2a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8be3a4917dba4ab244bdda26c4946feccf7dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
